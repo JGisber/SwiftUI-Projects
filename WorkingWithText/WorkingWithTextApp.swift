11fc5e6cc35686461cb7c7d44eff6d85049b9d6f
Josue Gisber <jgisber@gmail.com>
2020-10-29 17:09:17 -0600

    Initial Commit

---
+++ WorkingWithText/WorkingWithTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithTextApp.swift
+//  WorkingWithText
+//
+//  Created by Josue Gisber on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
